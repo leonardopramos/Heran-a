e2d512ac819f2e76f4f0c75960d470d3d89c5ca9
Leonardo Ramos <101880063+leonardopramos@users.noreply.github.com>
2022-09-12 20:32:29 -0300

    Add files via upload

---
+++ SolucaoExercicio/App2.java
@@ -0,0 +1,11 @@
+public class App2 {
+    
+    public static void main(String[] args) {
+        
+        FuncionarioAreaRisco fr = new FuncionarioAreaRisco("2202321", "Alexandre", "Cadeirante", 9999999.0);
+        System.out.println(fr);
+
+        Pesquisador ps = new Pesquisador("39042230", "Fabricio", 5748.0, 5);
+        System.out.println(ps);
+    }
+}
